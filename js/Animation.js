a9018f4a63dfb97372c7e385d71af0d7c431081e
John Skrzypek <jms8142@hotmail.com>
2012-03-06 12:22:24 -0500

    forgot to add animation.js

---
+++ js/Animation.js
@@ -0,0 +1,14 @@
+var WDAnimation = Class.create({
+		initialize : function () {}
+});
+
+WDAnimation.animateBlock = function(startPos,endPos){
+	console.info('static method');
+	console.info('from')
+	console.info(startPos);
+	console.info('to');
+	console.info(endPos);
+
+
+	/* http://www.8bitrocket.com/2009/05/03/tutorial-clearing-a-blit-canvas-by-erasing-only-the-portions-that-have-changed-using-damage-maps-or-a-dirty-rect/ */
+};
