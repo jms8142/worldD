308d2dca119543d73e0023abb47274f86b65995a
John Skrzypek <jms8142@hotmail.com>
2012-09-15 01:30:01 -0400

    added debugger.js

---
+++ js/Debugger.js
@@ -0,0 +1,26 @@
+var Debugger = Class.create({
+	initialize : function(){	
+		var _this = this;
+		jQuery('.step').live('click',function(e){
+			var ind = jQuery(this).attr('id') - 1;
+			_this.PrintGameBoardtoConsole(window._game.defaultSettings.gameRows,window._game.defaultSettings.columns,window._game.lastgameBoard[ind]);
+		});	
+	},
+	PrintGameBoardtoConsole : function(rows,cols,_gameBoard, clr){
+		if(clr)
+			console.clear();
+
+		for(var row = 0; row < rows; row++){
+			var lineout = '';
+			for(var col = 0; col < cols; col++){
+				lineout += _gameBoard[col][row].val + '|';
+			}
+			console.info('[row ' + (row + 1) + '] \t' +  lineout.substr(0,lineout.length-1));
+		}
+	},
+	updateSnapshotText : function(step){
+		jQuery('.utilinfo').append('<a class="step" href="#" id=' + step + '>' + step + '</a>');
+	}
+
+});
+
